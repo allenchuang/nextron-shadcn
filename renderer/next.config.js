1058c6fad33dded89a4b1b51e1d06b71662be73d
Allen Chuang <mr.allenchuang@gmail.com>
2025-05-07 00:25:36 -0700

    ignore VERCEL build

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  // Only enable static export for desktop builds
-  ...(process.env.BUILD_TARGET === 'desktop' ? { output: 'export' } : {}),
-  distDir: process.env.NODE_ENV === 'production' ? '../dist' : '.next',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  webpack: config => {
-    return config
-  }
+  // Only enable static export and custom distDir for non-Vercel builds
+  ...(process.env.VERCEL ? {} : {
+    output: 'export',
+    distDir: process.env.NODE_ENV === 'production' ? '../dist' : '.next',
+    trailingSlash: true,
+    images: {
+      unoptimized: true
+    },
+    webpack: config => {
+      return config
+    }
+  }),
+
 }
