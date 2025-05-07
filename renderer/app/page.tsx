bffee753c54edd00cb5f4f02c3814063e5c07822
Allen Chuang <mr.allenchuang@gmail.com>
2025-05-06 23:27:12 -0700

    Initial commit

---
+++ renderer/app/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
+function IndexPage() {
+  return (
+    <main className="flex flex-col items-center gap-2 pt-10">
+      <Image
+        priority
+        src="/images/logo.png"
+        alt="logo"
+        width={150}
+        height={150}
+      />
+      <h1 className="text-lg font-semibold">
+        Nextron ( Next.Js + Electron ) Boilerplate
+      </h1>
+      <Button>Click me</Button>
+      <p>With TypeScript, TailwindCSS and Shadcn/ui</p>
+      <p>Crossbuild for Web or Desktop</p>
+    </main>
+  )
+}
+
+export default IndexPage
